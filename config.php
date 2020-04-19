80485ec8ba78c0620095809f400ace13493ddcc0
Kevin Hughes <kevin.hughes@blueskyeducation.co.uk>
2020-04-19 14:11:49 +0100

    Https proxy setting if required

--- config.php
+++ config.php
@@ -18,7 +18,7 @@ $CFG->dboptions = array (
 );
 
 $CFG->wwwroot   = getenv('WWW_ROOT');
-$CFG->sslproxy  = true;
+$CFG->sslproxy  = substr($CFG->wwwroot, 0,5) == "https";
 $CFG->dataroot  = '/var/app/shared/moodledata';
 $CFG->admin     = 'admin';
 $CFG->tempdir   = '/var/app/tmp'; 
