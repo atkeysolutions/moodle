91651cdc71df2b344f841d89b0dfbdd03158456a
Kevin Hughes <kevin.hughes@blueskyeducation.co.uk>
2020-08-17 11:51:16 +0100

    Crap old blue auth script

---
+++ auth/blue_login.php
@@ -0,0 +1,19 @@
+<?php
+  require(__DIR__.'/../config.php');
+  require_once($CFG->dirroot.'/user/profile/lib.php');
+
+  $user = get_complete_user_data('id', $_GET['id']);
+
+  if($user->profile["blue_login_token"] == ""){
+    print_error("nologinas");
+  } else {
+    if($user->profile["blue_login_token"] == $_GET['token']){
+      complete_user_login($user);
+      $user->profile_field_blue_login_token = "";
+      profile_save_data($user);
+      redirect($_GET['redirect'] ? $_GET['redirect'] : "/");
+    } else {
+      print_error("nologinas");
+    }
+  }
+?>
