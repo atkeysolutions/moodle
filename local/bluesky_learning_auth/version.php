60a6c798e1b3672395c92ecb8a3c241868315f88
Kevin Hughes <kevin.hughes@blueskyeducation.co.uk>
2020-09-16 21:44:53 +0100

    Bluesky Learning can now log you out

--- local/bluesky_learning_auth/version.php
+++ local/bluesky_learning_auth/version.php
@@ -1,7 +1,6 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = '0.2';
+$plugin->version = '0.3';
 $plugin->component = 'local_bluesky_learning_auth';
 $plugin->maturity = MATURITY_STABLE;
-
