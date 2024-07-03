58dee3145d50e313b999ca3b63106e5433f8b32c
Adam Barker <adam.barker@tintisha.net>
2024-07-03 15:28:52 +0100

    Addition of backdoor URL in description

--- local/bluesky_learning_auth/settings.php
+++ local/bluesky_learning_auth/settings.php
@@ -16,7 +16,7 @@ if ( $hassiteconfig ){
 	$settings->add( new admin_setting_configcheckbox(
 		'local_bluesky_learning_auth/backdoor_active',
 		get_string('backdoor_active','local_bluesky_learning_auth'),
-		get_string('backdoor_active_help','local_bluesky_learning_auth'),
+		get_string('backdoor_active_help','local_bluesky_learning_auth',$CFG->wwwroot),
 		0
 	) );
 
